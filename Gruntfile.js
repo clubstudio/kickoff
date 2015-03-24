280577b734adce25ce40c902c5a0c583a2ec2931
Scott Wakefield <scott@clubstudio.co.uk>
2015-03-24 11:46:29 +0000

    Add cssmin task to build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -180,10 +180,11 @@ module.exports = function(grunt) {
         grunt.loadNpmTasks('grunt-contrib-copy');
         grunt.loadNpmTasks('grunt-sass');
         grunt.loadNpmTasks('grunt-autoprefixer');
+        grunt.loadNpmTasks('grunt-contrib-cssmin');
         grunt.loadNpmTasks('grunt-contrib-uglify');
         grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-        grunt.task.run('copy', 'sass', 'autoprefixer', 'uglify', 'imagemin');
+        grunt.task.run('copy', 'sass', 'autoprefixer', 'cssmin', 'uglify', 'imagemin');
     });
 
 }
