4ec0dd81f32213a13d4f7040d2a3545695db29e8
Scott Wakefield <scott@clubstudio.co.uk>
2015-05-28 15:08:49 +0000

    Updates and tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,16 @@
-var config     = require('./project.json'),
-    gulp       = require('gulp'),
+/* --------------------------------------------------------------------------
+ * Get project configuration
+ * -------------------------------------------------------------------------- */
+
+var config     = require('./project.json');
+
+/* --------------------------------------------------------------------------
+ * Require Tasks
+ * -------------------------------------------------------------------------- */
+
+var gulp       = require('gulp'),
     del        = require('del'),
-    sass       = require('gulp-sass')
+    sass       = require('gulp-sass'),
     prefixer   = require('gulp-autoprefixer'),
     minifyCss  = require('gulp-minify-css'),
     uglify     = require('gulp-uglify'),
@@ -16,14 +25,22 @@ var config     = require('./project.json'),
     watch      = require('gulp-watch'),
     livereload = require('gulp-livereload');
 
+/* --------------------------------------------------------------------------
+ * Clear any previously built files before rebuilding assets
+ * -------------------------------------------------------------------------- */
+
 gulp.task('clean', function () {
-    return del([
+    return del.sync([
         config.dirs.build.css,
         config.dirs.build.js,
         config.dirs.build.img,
-        config.manifest,
+        config.manifest
     ]);
 });
+
+/* --------------------------------------------------------------------------
+ * Copy and minify any bower components
+ * -------------------------------------------------------------------------- */
 
 gulp.task('copy-components', ['clean'], function () {
     return gulp.src(config.components, { cwd: config.dirs.components })
@@ -31,30 +48,46 @@ gulp.task('copy-components', ['clean'], function () {
         .pipe(gulp.dest(config.dirs.build.js + '/vendor'));
 });
 
-gulp.task('sass', ['copy-components'], function () {
-    return gulp.src(config.dirs.assets.sass + '/**/*.scss')
+/* ---------------------------------------------------------------------------
+ * Compile SASS into CSS
+ * -------------------------------------------------------------------------- */
+
+gulp.task('sass', function () {
+    return gulp.src(config.dirs.assets.sass + '/**/*.{scss,sass}')
         .pipe(sass())
         .pipe(gulp.dest(config.dirs.build.css))
         .pipe(livereload());
 });
 
-gulp.task('autoprefixer', ['sass'], function () {
+/* --------------------------------------------------------------------------
+ * Add vendor prefixes to compiled css
+ * -------------------------------------------------------------------------- */
+
+gulp.task('autoprefixer', function () {
     return gulp.src(config.dirs.build.css + '/**/*.css')
         .pipe(prefixer({
-             browsers: ['last 2 versions', 'ie 8', 'ie 9']
-         }))
+            browsers: ['last 2 versions', 'ie 8', 'ie 9']
+        }))
         .pipe(gulp.dest(config.dirs.build.css));
 });
 
-gulp.task('minify-css', ['sass', 'autoprefixer'], function () {
+/* --------------------------------------------------------------------------
+ * Minify all CSS files
+ * -------------------------------------------------------------------------- */
+
+gulp.task('minify-css', function () {
     return gulp.src(config.dirs.build.css + '/**/*.css')
         .pipe(minifyCss({compatibility: 'ie8'}))
         .pipe(gulp.dest(config.dirs.build.css));
 });
 
+/* --------------------------------------------------------------------------
+ * Concatenate and minify Javascript
+ * -------------------------------------------------------------------------- */
+
 gulp.task('uglify', function () {
     for (var key in config.js) {
-            gulp.src(config.js[key])
+        gulp.src(config.js[key])
             .pipe(uglify())
             .pipe(concat(key))
             .pipe(gulp.dest(config.dirs.build.js))
@@ -62,22 +95,38 @@ gulp.task('uglify', function () {
     }
 });
 
-gulp.task('imagemin', ['copy-components'], function () {
-    return gulp.src(config.dirs.assets.img + '/**/*')
-               .pipe(imagemin({
-                   progressive: true,
-                   svgoPlugins: [{ removeViewBox: false}],
-                   use: [pngquant()]
-               }))
-               .pipe(gulp.dest(config.dirs.build.img));
+/* --------------------------------------------------------------------------
+ * Optimise images
+ * -------------------------------------------------------------------------- */
+
+gulp.task('imagemin', function () {
+    gulp.src(config.dirs.assets.img + '/**/*')
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest(config.dirs.build.img));
 });
 
-gulp.task('rev', ['clean', 'copy-components', 'minify-css', 'uglify', 'imagemin'], function () {
-    return gulp.src([
-            config.dirs.build.css + '/**/*.css',
-            config.dirs.build.js + '/**/*.js',
-            config.dirs.build.img + '/**/*.{png,jpg,gif,svg}'
-        ], { base: path.join(process.cwd(), config.dirs.build.root) })
+/* --------------------------------------------------------------------------
+ * Add hash to built file names
+ *     1. Rev filenames
+ *     2. Override filenames in CSS files
+ *     3. Generate assets manifest file
+ * -------------------------------------------------------------------------- */
+
+gulp.task('rev', function () {
+    return gulp.src(
+            [
+                config.dirs.build.css + '/**/*.css',
+                config.dirs.build.js + '/**/*.js',
+                config.dirs.build.img + '/**/*.{png,jpg,gif,svg}'
+            ],
+            {
+                base: path.join(process.cwd(), config.dirs.build.root)
+            }
+        )
         .pipe(rev())
         .pipe(override())
         .pipe(gulp.dest(config.dirs.build.root))
@@ -86,28 +135,64 @@ gulp.task('rev', ['clean', 'copy-components', 'minify-css', 'uglify', 'imagemin'
         .pipe(gulp.dest(config.dirs.assets.root));
 });
 
+/* --------------------------------------------------------------------------
+ * Live reload any changes to templates/views
+ * -------------------------------------------------------------------------- */
+
 gulp.task('templates', function () {
     return gulp.src(config.dirs.templates + '/**/*.{html,twig,php}')
         .pipe(livereload());
 });
 
+/* --------------------------------------------------------------------------
+ * Watch for any file changes
+ * -------------------------------------------------------------------------- */
+
 gulp.task('watch', function () {
     livereload.listen();
 
-	gulp.watch(config.dirs.assets.sass + '/**/*.{scss,sass}', ['sass']);
-	gulp.watch(config.dirs.assets.js + '/**/*.js', ['uglify']);
-	gulp.watch(config.dirs.assets.img + '/**/*.{png,jpg,gif,svg}', ['imagemin']);
-	gulp.watch(config.dirs.templates + '/**/*.{html,twig,php}', ['templates']);
+    gulp.watch(config.dirs.assets.sass + '/**/*.{scss,sass}', ['sass']);
+    gulp.watch(config.dirs.assets.js + '/**/*.js', ['uglify']);
+    gulp.watch(config.dirs.assets.img + '/**/*.{png,jpg,gif,svg}', ['imagemin']);
+    gulp.watch(config.dirs.templates + '/**/*.{html,twig,php}', ['templates']);
 });
 
-gulp.task('build', [
-    'clean',
-    'copy-components',
-    'sass',
-    'autoprefixer',
-    'minify-css',
-    'uglify',
-    'rev'
-]);
+/* --------------------------------------------------------------------------
+ * Build - Builds all assets ready for production
+ *     1. Removes any previously built assets
+ *     2. Copies any required dependencies
+ *     3. Compiles CSS
+ *     4. Applies any vendor prefixes
+ *     5. Minifies CSS
+ *     6. Builds JS files
+ *     7. Optimises images
+ *     8. Add hashes to asset filenames
+ * -------------------------------------------------------------------------- */
 
-gulp.task('default', ['clean', 'copy-components', 'sass', 'uglify', 'watch']);
+gulp.task('build', function () {
+    return sequence(
+        'clean',
+        'copy-components',
+        ['sass', 'imagemin', 'uglify'],
+        'autoprefixer',
+        'minify-css',
+        'rev'
+    )
+});
+
+/* --------------------------------------------------------------------------
+ * Default Task
+ *     1. Cleans any previously built assets
+ *     2. Copies any required dependencies
+ *     3. Builds the development versions of any css, javascript or images
+ *     4. Watches for any changes...
+ * -------------------------------------------------------------------------- */
+
+gulp.task('default', function () {
+    return sequence(
+        'clean',
+        'copy-components',
+        ['sass', 'uglify', 'imagemin'],
+        'watch'
+    );
+});
